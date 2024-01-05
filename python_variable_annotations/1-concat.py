f017e02059df17a07383a9c86b709f73e4f23437
Ishay Junnco <6905@holbertonstudents.com>
2024-01-05 12:08:16 -0800

    fixed python style

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Variable Annotations"""
 
-def concat (str1: str, str2: str) ->str:
+
+def concat(str1: str, str2: str) -> str:
     """Return a concatenated string"""
     return str1+str2
-
