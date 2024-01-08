957e1b52ba4f0ceca755b438a080e8d7df37674e
Ishay Junnco <6905@holbertonstudents.com>
2024-01-08 12:56:20 -0800

    Task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """Variable Annotations"""
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
-
-    """Return """
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return the sum of a list float or int number"""
+    suma: float = 0
+    for num in mxd_lst:
+        suma = suma + num
+    return suma
