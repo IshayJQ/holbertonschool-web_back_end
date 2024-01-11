e2fea255832cac96677a6cd96cab91fa13e37c24
Ishay Junnco <6905@holbertonstudents.com>
2024-01-10 17:18:53 -0800

    Fix match documents task 10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,12 +6,7 @@ changes all topics of a document based on the name
 
 def update_topics(mongo_collection, name, topics):
     """function that changes all topics"""
-    topic = mongo_collection.update_one(
+    topic = mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
     )
-
-    if topic.modified_count > 0:
-        return True
-    else:
-        return False
