9dfbe4d40bf1629a078dd4cbd71d2c6d1d8c820e
Ishay Junnco <6905@holbertonstudents.com>
2024-01-22 08:18:51 -0800

    Fix typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,15 +2,16 @@
 
 import asyncio
 import random
+from typing import List
 """
 Coroutine that loops 10 times, asynchronously waits 1 second,
 and yields a random number between 0 and 10.
 """
 
 
-async def async_generator():
+async def async_generator() -> List[float]:
     """
-    Return The random number.
+    Generate the random number.
     """
     for loops in range(10):
         await asyncio.sleep(1)
