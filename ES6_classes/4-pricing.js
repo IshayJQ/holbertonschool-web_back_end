d101ae6f091c68cc9e51c7b56b4e0e1db8b3df99
Ishay Junnco <6905@holbertonstudents.com>
2024-01-29 16:39:26 -0800

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('amount and conversionRate mut be number');
+    }
+    return amount * conversionRate;
+  }
+}
