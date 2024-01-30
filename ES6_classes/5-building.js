942129ea921b994ee19e8b20ccb21c7e37018749
Ishay Junnco <6905@holbertonstudents.com>
2024-01-29 18:00:27 -0800

    Fix task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    // Intentionally not using 'this' because it's an abstract method
-    throw new TypeError('Class extending Building must override evacuationWarningMessage');
-  }
-  /* eslint-enable class-methods-use-this */
 }
