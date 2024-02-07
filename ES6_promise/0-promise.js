148ca67039114e81186c3fb65ae218ceb1baa839
Holberton iMac 17 <holbertonimac17@Holbertons-iMac.local>
2024-02-07 16:47:18 -0500

    Fix use reject

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("foo");
-    }, 300);
-    });
+  return new Promise(() => {});
 }
