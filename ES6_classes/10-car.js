fb4a4d508c13ecaa7270a9a8d387e7c2ab17648e
Ishay <ijunnco@hotmail.com>
2024-02-16 22:11:30 -0500

    Fix code

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -5,7 +5,31 @@ export default class Car {
     this.color = color;
   }
 
-static get [Symbol.species]() {
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
     return this;
   }
 
