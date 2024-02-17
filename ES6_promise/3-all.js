55871fcb7954f24b82bec364baeb4648fe985b5f
Holberton iMac 17 <holbertonimac17@Holbertons-iMac.local>
2024-02-16 19:42:14 -0500

    fix return task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   // Promise.all to handle promises collectively
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
